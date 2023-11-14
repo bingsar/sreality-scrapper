71d7ff0a26c28faecdd936924163c09c9f50a505
Anton Shmigelskiy <bingsa@yandex.ru>
2023-11-14 12:27:58 +0100

    chore: fetching only current page

--- server/server.js
+++ server/server.js
@@ -1,60 +1,54 @@
-const express = require('express');
-const cors = require('cors');
-const {pool: pool} = require('./db.js');
-const {scrapper: scrapper} = require('./scrapper.js');
+const express = require('express')
+const cors = require('cors')
+const {pool: pool} = require('./db.js')
+const {scrapper: scrapper} = require('./scrapper.js')
 
-const app = express();
-const port = 3001;
-app.use(cors());
+const app = express()
+const port = 3001
+app.use(cors())
 
-scrapper()
+scrapper().then(() => console.log('Scrapper finished'))
 
 app.get('/api/properties', async (req, res) => {
     try {
+        const { page = 1, itemsPerPage = 20 } = req.query
+        const offset = (page - 1) * itemsPerPage
+
         const result = await pool.query(`
-            SELECT 
-                p.property_id,
-                p.title,
-                i.url
-            FROM 
-                property p
-            LEFT JOIN 
-                image i ON p.property_id = i.property_id
-        `);
+            SELECT p.property_id, p.title, ARRAY_AGG(i.url) AS images
+            FROM property p
+            LEFT JOIN image i ON p.property_id = i.property_id
+            GROUP BY p.property_id, p.title
+            ORDER BY p.property_id
+            OFFSET $1
+            LIMIT $2;
+        `, [offset, itemsPerPage])
 
-        const properties = result.rows.reduce((acc, row) => {
-            const property = acc.find(item => item.property_id === row.property_id);
-            if (property) {
-                property.images.push(row.url);
-            } else {
-                acc.push({
-                    property_id: row.property_id,
-                    title: row.title,
-                    images: row.url ? [row.url] : [],
-                });
-            }
-            return acc;
-        }, []);
+        const properties = result.rows.map(row => ({
+            property_id: row.property_id,
+            title: row.title,
+            images: row.images.filter(url => url !== '')
+        }));
 
-        res.json(properties);
+        res.json(properties)
     } catch (error) {
-        console.error('Error fetching properties:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error fetching properties:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 });
 
 app.get('/api/propertyCount', async (req, res) => {
     try {
-        const result = await pool.query('SELECT COUNT(*) FROM property');
-        const propertyCount = result.rows[0].count;
+        const result = await pool.query('SELECT COUNT(*) FROM property')
+        const propertyCount = result.rows[0].count
 
-        res.json({ propertyCount });
+        res.json({ propertyCount })
     } catch (error) {
-        console.error('Error fetching property count:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error fetching property count:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
     }
-});
+})
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Server is running on http://localhost:${port}`)
+})
